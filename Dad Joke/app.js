23d1f3925b3edbce1683d555a4345c01c05131b0
rukmanpalta <rukmanpalta@gmail.com>
2022-03-07 23:49:33 -0500

    "commit"

---
+++ Dad Joke/app.js
@@ -0,0 +1,26 @@
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+const url = "https://icanhazdadjoke.com/";
+btn.addEventListener("click", () => {
+  fetchDadJoke();
+});
+
+const fetchDadJoke = async () => {
+  result.textContent = "Loading...";
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Accept: "application/json",
+        "User-Agent": "learning app",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("error");
+    }
+    const data = await response.json();
+    result.textContent = data.joke;
+  } catch (error) {
+    console.log(error.message);
+    result.textContent = "There was an error...";
+  }
+};
